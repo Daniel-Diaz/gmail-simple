4e22c9d6711311848511a00801a660036c5d1d83
Daniel Casanueva <dhelta.diaz@gmail.com>
2022-02-13 14:34:44 +0100

    Add custom build script.

---
+++ build-script.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# This script is used to build the code against several sets of dependencies.
+# We use stack for this.
+
+# This makes the build script stop after the first error.
+set -e
+
+# Function to build the code using the resolver passed as argument.
+function build {
+echo -e "\e[92m>> Building using resolver $1...\e[0m"
+stack --resolver=$1 build
+}
+
+# Run build command for all the resolvers we want to support.
+build lts-17
+build lts-18
+build nightly
